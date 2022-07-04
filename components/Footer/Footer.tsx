e0bf63290cea412ba9deaed62d32a6eec0fe4036
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-07-04 16:47:29 +0100

    three piece footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
 import styles from './Footer.module.css'
  
 export const Footer = () => {
     return (
-        <div className=''>this is Footer</div>
+        <div className={styles.container}>
+            <p>&copy; 2022 Ingen | 0xreeko</p>
+            <p>Made with 👾 with Nextjs & TailwindCSS</p>
+            <p>Dev'd by <span className={styles.link}><Link href={'https://www.twitter.com/0xreeko'} target="_blank" rel="noopener">0xreeko</Link></span></p>
+        </div>
     )
 };
