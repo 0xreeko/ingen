7cc6c47883bdd549f019acefc4ba5ccce189ff30
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-07-04 16:53:46 +0100

    added base container styles for header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,6 +2,8 @@ import styles from './Header.module.css'
  
 export const Header = () => {
     return (
-        <div className=''>this is Header</div>
+        <div className={styles.container}>
+            <h1>ingen</h1>
+        </div>
     )
 };
