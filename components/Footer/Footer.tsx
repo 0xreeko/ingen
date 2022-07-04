6ce057f6cf8dcf063a4ea4b5952515fe7074c9bb
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-07-04 16:49:13 +0100

    removed alias from copy section

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.css'
 export const Footer = () => {
     return (
         <div className={styles.container}>
-            <p>&copy; 2022 Ingen | 0xreeko</p>
+            <p>&copy; 2022 Ingen</p>
             <p>Made with 👾 with Nextjs & TailwindCSS</p>
             <p>Dev'd by <span className={styles.link}><Link href={'https://www.twitter.com/0xreeko'} target="_blank" rel="noopener">0xreeko</Link></span></p>
         </div>
