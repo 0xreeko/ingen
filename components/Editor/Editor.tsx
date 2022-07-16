85f5cc4c16c74f2d31bdcb80626faa095e727fb6
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-07-16 15:29:22 +0100

    added editor plus A4 dimensions and placeholder

--- components/Editor/Editor.tsx
+++ components/Editor/Editor.tsx
@@ -2,6 +2,15 @@ import styles from './Editor.module.css'
  
 export const Editor = () => {
     return (
-        <div className={styles.container}>this is Editor</div>
+        <div className={styles.container} contentEditable>
+            <p className='font-bold desktop-h4'>Johan Doe</p>
+            <div className='flex divide-x-[1px] divide-sylver-800'>
+            <p className='pr-2'>London, UK</p>
+            <p className='px-2'>+4478038092309</p>
+            <p className='px-2'>johandoe@explain.com</p>
+            <p className='pl-2'>johandoe.xyz</p>
+            </div>
+            <div className={styles.header} contentEditable={false} />
+        </div>
     )
 };
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,9 +1,12 @@
 import styles from './Hero.module.css'
+import Typewriter from 'typewriter-effect'
+
  
 export const Hero = () => {
     return (
         <div className={styles.container}>
-            <h1 className={styles.hero}>Spin up your own digital documents the supasonic way.</h1>
+            <h1 className={`desktop-hero ${styles.hero}`}>Build <span className={styles.twriter}>{<Typewriter options={{strings: ['an invoice.', 'a resume.', 'a fillable PDF.', 'an order.', 'a contract.', 'a certificate.', 'a report.', 'a letter.', 'an agreement.', 'a ticket.'], deleteSpeed: 40, autoStart: true, loop: true}}/>}</span></h1>
+            <h2 className={`desktop-b2 ${styles.subhead}`}>Generate digital docs the <span className={styles.supasonic}>supasonic</span> way.</h2>
         </div>
     )
 };
